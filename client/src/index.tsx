5506f5ea5f75baf7f092693c4bcdabe1d7cc952b
chiranz <chiranpoudyal@gmail.com>
2019-09-29 20:06:26 +0530

    auto refresh access token  within session

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,24 +1,95 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { getAccessToken, setAccessToken } from "./accessToken";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink, Observable } from "apollo-link";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwtDecode from "jwt-decode";
 import { App } from "./App";
-import { getAccessToken } from "./accessToken";
+
+const cache = new InMemoryCache({});
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable(observer => {
+      let handle: any;
+      Promise.resolve(operation)
+        .then(operation => {
+          const accessToken = getAccessToken();
+          if (accessToken) {
+            operation.setContext({
+              headers: {
+                authorization: `bearer ${accessToken}`
+              }
+            });
+          }
+        })
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer)
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
 
 const client = new ApolloClient({
-  uri: "http://localhost:3030/graphql",
-  credentials: "include",
-  request: operation => {
-    const accessToken = getAccessToken();
-    if (accessToken) {
-      operation.setContext({
-        headers: {
-          authorization: `bearer ${accessToken}`
+  link: ApolloLink.from([
+    new TokenRefreshLink({
+      accessTokenField: "accessToken",
+      isTokenValidOrUndefined: () => {
+        const token = getAccessToken();
+        if (!token) {
+          return false;
         }
-      });
-    }
-  }
+        try {
+          const { exp } = jwtDecode(token);
+          if (Date.now() >= exp * 1000) {
+            return false;
+          } else {
+            return true;
+          }
+        } catch {
+          return false;
+        }
+      },
+      fetchAccessToken: () => {
+        return fetch("http://localhost:3030/refresh_token", {
+          credentials: "include",
+          method: "post"
+        });
+      },
+      handleFetch: accessToken => {
+        setAccessToken(accessToken);
+      },
+
+      handleError: err => {
+        console.warn("Your refresh token is invalid. Try to relogin");
+        console.log(err);
+      }
+    }),
+    onError(({ graphQLErrors, networkError }) => {
+      console.log(graphQLErrors);
+      console.log(networkError);
+    }),
+    requestLink,
+    new HttpLink({
+      uri: "http://localhost:3030/graphql",
+      credentials: "include"
+    })
+  ]),
+  cache
 });
 
 ReactDOM.render(
--- client/src/pages/Bye.tsx
+++ client/src/pages/Bye.tsx
@@ -4,7 +4,7 @@ import { useByeQuery } from "../generated/graphql";
 interface Props {}
 
 export const Bye: React.FC<Props> = () => {
-  const { data, loading, error } = useByeQuery();
+  const { data, loading, error } = useByeQuery({ fetchPolicy: "network-only" });
   if (loading) {
     return <h1>Loading...</h1>;
   }
